75d2b1c6bead65a44f019a5fb0e6de32542e7293
Patrick Ester <patricke@codio.com>
2021-02-05 19:47:56 +0000

    Erase code from Java file

--- code/mutability/ExternalMethods.java
+++ code/mutability/ExternalMethods.java
@@ -1,16 +1,6 @@
 //add class definitions below this line
 
-class Player {
-  int health;
-  int score;
-  int level;
-  
-  Player() {
-    health = 100;
-    score = 0;
-    level = 1;
-  }
-}    
+    
  
 //add class definitions above this line
 
@@ -19,25 +9,14 @@ public class ExternalMethods {
     
     //add code below this line
 
-    Player player1 = new Player();
-    System.out.println(printPlayer(player1));
-    player1.health = 0;
-    player1.score += 25;
-    player1.level += 1;
-    System.out.println(printPlayer(player1));
+
 
     //add code above this line
   }
   
   //add method definitions below this line
   
-  public static String printPlayer(Player p) {
-    if (p.health <= 0) {
-      return "This player is dead. They died on level " + p.level + " with a score of " + p.score + ".";
-    } else {
-      return "This player has " + p.health + " health, a score of " + p.score + " and is on level " + p.level + ".";
-    }
-  }
+
   
   //add method definitions above this line
 }
