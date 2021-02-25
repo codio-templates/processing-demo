a724c60d897a32e2deeeef58bcf60a6b4c92efa7
Patrick Ester <patricke@codio.com>
2021-02-25 21:21:32 +0000

    Finish page on private keyword

--- code/encapsulation/PrivateKeyword.java
+++ code/encapsulation/PrivateKeyword.java
@@ -4,12 +4,12 @@
   
 //add class definitions above this line
 
-public class PublicKeyword {
+public class PrivateKeyword {
   public static void main(String[] args) {
     
     //add code below this line
 
-    
+
 
     //add code above this line
   }
