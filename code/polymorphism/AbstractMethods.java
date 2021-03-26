efaf594e449a40fec81b73193a1c8d4acb3a44ae
Patrick Ester <patricke@codio.com>
2021-03-26 21:26:46 +0000

    Finish page on abstract methods

--- code/polymorphism/AbstractMethods.java
+++ code/polymorphism/AbstractMethods.java
@@ -1,4 +1,4 @@
-abstract class Person {
+class Person {
   private String name;
   
   public String getName() {
@@ -8,37 +8,11 @@ abstract class Person {
   public void setName(String newName) {
     name = newName;
   }
-  
-  public abstract String greeting();
 }
 
 //add class definitions below this line
 
-class Engineer extends Person {
-  private String speciality;
-  
-  public Engineer(String n, String s) {
-    setName(n);
-    speciality = s;
-  }
-  
-  public String greeting() {
-    return "Hello, my name is " + getName() + " and I am a(n) " + speciality;
-  }
-}
 
-class Artist extends Person {
-  private String medium;
-  
-  public Artist(String n, String m) {
-    setName(n);
-    medium = m;
-  }
-  
-  public String greeting() {
-    return "My name is " + getName() + " and I work with " + medium;
-  }
-}
 
 //add class definitions above this line
 
@@ -47,11 +21,7 @@ public class AbstractMethods {
     
     //add code below this line
 
-    Person p = new Engineer("Calvin", "civil engineer");
-    Person a = new Artist("Maria", "water colors");
-    
-    System.out.println(p.greeting());
-    System.out.println(a.greeting());
+
     
     //add code above this line
   }
