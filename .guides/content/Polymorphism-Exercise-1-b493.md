43f9d1897e30e43f0530e0c8b1b283fc9c19fc47
Patrick Ester <patricke@codio.com>
2021-03-31 19:27:58 +0000

    Finish coding exercise 1

--- .guides/content/Polymorphism-Exercise-1-b493.md
+++ .guides/content/Polymorphism-Exercise-1-b493.md
@@ -2,3 +2,8 @@
 
 ## Exercise 1
 
+Use the `Lottery` class to the left as the superclass. Create the `PowerBall` class as a subclass of `Lottery`. Override the `shuffle` method so that it returns an ArrayList of six random integers up to but not including 100.
+
+{Try it}(sh .guides/bg.sh javac code/polymorphism/exercise1/Exercise1.java java -cp code/polymorphism/exercise1/ Exercise1 1)
+
+{Check It!|assessment}(test-1690807397)
--- code/polymorphism/exercise1/Exercise1.java
+++ code/polymorphism/exercise1/Exercise1.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+import java.util.Random; 
+
+class Lottery {
+  public ArrayList<Integer> shuffle() {
+    Random r = new Random();
+    ArrayList<Integer> nums = new ArrayList<Integer>();
+    for (int i = 0; i < 5; i++) {
+      int num = r.nextInt(20);
+      nums.add(num);
+    }
+    return nums;
+  }
+}
+
 //add class definitions below this line
 
 
@@ -9,7 +24,7 @@ public class Exercise1 {
     
     //add code below this line
 
-
+    
     
     //add code above this line
   }
