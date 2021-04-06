794e23732788ba933b5ac637b72621701086665f
Patrick Ester <patricke@codio.com>
2021-04-06 16:18:19 +0000

    Start page on interfaces

--- .guides/content/Interfaces-2416.md
+++ .guides/content/Interfaces-2416.md
@@ -1,1 +1,7 @@
-# Sample content New Page
+----------
+
+## Interfaces
+
+Interfaces are similar to abstract classes in that they cannot be instantiated and methods must be defined by subclasses.
+
+{Try it}(sh .guides/bg.sh javac code/advanced/Interfaces.java java -cp code/advanced/ Interfaces 1)
---
+++ code/advanced/Interfaces.java
@@ -0,0 +1,19 @@
+//add class definitions below this line
+
+interface Dog {
+  String bark();
+}
+ 
+//add class definitions above this line
+
+public class Interfaces {  
+  public static void main(String[] args) {
+    
+  //add code below this line
+
+  
+
+  //add code above this line
+    
+  }
+}
