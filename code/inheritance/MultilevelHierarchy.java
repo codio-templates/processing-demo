4aa418da941fa078ed55093197bdf6848e34e866
Patrick Ester <patricke@codio.com>
2021-04-06 18:01:45 +0000

    Second round of QA edits

--- code/inheritance/MultilevelHierarchy.java
+++ code/inheritance/MultilevelHierarchy.java
@@ -1,9 +1,6 @@
 //add class definitions below this line
 
-class ClassA {}
-class ClassB extends ClassA {}
-class ClassC {}
-class ClassD extends ClassB {}
+
  
 //add class definitions above this line
 
@@ -12,8 +9,7 @@ public class MultilevelHierarchy {
     
     //add code below this line
   
-    ClassD d = new ClassD();
-    System.out.println(d instanceof ClassC);
+    
   
     //add code above this line      
   }
