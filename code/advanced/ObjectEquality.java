a26d00688bd0a1ca510637a2abfa49094c724645
Patrick Ester <patricke@codio.com>
2021-04-07 18:52:22 +0000

    Finish page on object equality

--- code/advanced/ObjectEquality.java
+++ code/advanced/ObjectEquality.java
@@ -1,19 +1,17 @@
-interface Dog {
-  String bark();
-}
-
 //add class definitions below this line
 
 
+  
+}
  
 //add class definitions above this line
 
-public class Interfaces {  
+public class ObjectEquality {  
   public static void main(String[] args) {
     
     //add code below this line
 
-    
+
 
     //add code above this line
     
