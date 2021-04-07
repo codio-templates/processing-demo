2d7e914f5d2f1b574f7c1e3f4963a1b00ba2924a
Patrick Ester <patricke@codio.com>
2021-04-07 21:58:45 +0000

    Finish lab challenge

---
+++ .guides/secure/advanced/LabChallengeTester.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class LabChallengeTester {
+  
+  @Test
+  public void checkToString1() {
+    BankAccount ba = new BankAccount(1234.56, 7890.12);
+    String expected = "BankAccount[checking=1234.56, savings=7890.12]";
+    String actual = ba.toString();
+    String feedback = "Test did not pass.";
+    assertEquals(feedback, expected, actual);
+  }
+  
+  @Test
+  public void checkToString2() {
+    BankAccount ba = new BankAccount(3325.45, 1618.89);
+    String expected = "BankAccount[checking=3325.45, savings=1618.89]";
+    String actual = ba.toString();
+    String feedback = "Test did not pass.";
+    assertEquals(feedback, expected, actual);
+  }
+  
+}
---
+++ code/advanced/labChallenge/LabChallenge.java
@@ -0,0 +1,28 @@
+class BankAccount {
+  private double checking;
+  private double savings;
+  
+  public BankAccount(double c, double s) {
+    checking = c;
+    savings = s;
+  }
+  
+  //add toString definition below this line
+  
+  
+  
+  //add toString definition above this line
+  
+}
+
+public class LabChallenge {  
+  public static void main(String[] args) {
+    
+    //add code below this line
+
+    
+
+    //add code above this line
+    
+  }
+}
